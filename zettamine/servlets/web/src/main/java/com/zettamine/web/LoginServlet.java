3cca768dac2aba4f01a10e028e7a2d18353c9496
suniledureka <sunil.edureka@gmail.com>
2023-12-27 12:04:21 +0530

    servlets

--- zettamine/servlets/web/src/main/java/com/zettamine/web/LoginServlet.java
+++ zettamine/servlets/web/src/main/java/com/zettamine/web/LoginServlet.java
@@ -13,6 +13,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -39,6 +40,9 @@ public class LoginServlet extends HttpServlet {
 			
 			ResultSet rs = pst.executeQuery();
 			if(rs.next()) {
+				HttpSession session = request.getSession();
+				session.setAttribute("login", 1);
+				
 				RequestDispatcher rd = request.getRequestDispatcher("welcome");
 				rd.forward(request, response);
 			}else {
--- zettamine/servlets/web/src/main/java/com/zettamine/web/WelcomeServlet.java
+++ zettamine/servlets/web/src/main/java/com/zettamine/web/WelcomeServlet.java
@@ -1,31 +1,47 @@
 package com.zettamine.web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import jakarta.servlet.http.HttpSession;
+
 @WebServlet("/welcome")
 public class WelcomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		
-		out.println("<html>");
-		out.println("<body style=\"text-align:center;background:linear-gradient(to right, orange, white, green)\">");
-		out.println("<h1 style=\"text-shadow:0 0 10px blue\">ZETTAMINE LABS PRIVATE LIMITED</h1>");
-		out.println("<img src=images/zetta.jpg width=400px style=\"box-shadow: 0px 0px 20px black; border-radius:20px\">");
-		out.println("<hr>");
-		out.println("<p style=text-align:left>Hello <ins>" + request.getParameter("txt_uid")+"</ins></p>");
-		out.println("</body>");
-		out.println("</html>");
+
+		HttpSession session = request.getSession();
+
+		if (session.getAttribute("login")!=null) {
+			out.println("<html>");
+			out.println(
+					"<body style=\"text-align:center;background:linear-gradient(to right, orange, white, green)\">");
+			out.println("<h1 style=\"text-shadow:0 0 10px blue\">ZETTAMINE LABS PRIVATE LIMITED</h1>");
+			out.println(
+					"<img src=images/zetta.jpg width=400px style=\"box-shadow: 0px 0px 20px black; border-radius:20px\">");
+			out.println("<hr>");
+			out.println("<p style=text-align:left>Hello <ins>" + request.getParameter("txt_uid") + "</ins></p>");
+			out.println("</body>");
+			out.println("</html>");
+		}else {
+			request.setAttribute("message", "please login first");
+			RequestDispatcher rd = request.getRequestDispatcher("login.html");
+			rd.forward(request, response);
+		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
