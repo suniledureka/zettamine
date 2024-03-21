91049809095507f76a8fbee4ded696a827f8e494
suniledureka <sunil.edureka@gmail.com>
2024-03-21 13:01:26 +0530

    ms

--- zettamine/grafana/accounts/src/main/java/com/zettamine/accounts/service/ICustomerService.java
+++
@@ -1,7 +0,0 @@
-package com.zettamine.accounts.service;
-
-import com.zettamine.accounts.dto.CustomerDetailsDto;
-
-public interface ICustomerService {
-	CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId);
-}
