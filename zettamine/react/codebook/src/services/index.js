2059f0e27477cc5fc8687d847b66311380ea749d
suniledureka <sunil.edureka@gmail.com>
2024-04-26 18:45:18 +0530

    react

--- zettamine/react/codebook/src/services/index.js
+++
@@ -1,3 +0,0 @@
-export { login, register, logout } from "./authService";
-export { getUser, getUserOrders, createOrder } from "./dataService";
-export { getProductList, getProduct, getFeaturedList } from "./productService";
